64609f39e8b9e2a1097eef905e45d2329e6d54f6
Miller, Stuart (S&T-Student) <sm67c@mst.edu>
2017-09-09 12:34:06 -0500

    Configure each input file as specified in Assignment 1a.

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Compile project
+# Note: Compiling with C++11 to make use of high_resolution_clock for microsecond seeding
 g++ src/*.cpp -std=c++11 -o stock_cutting.exe
+
+# Run with given configuration
 ./stock_cutting.exe $1
