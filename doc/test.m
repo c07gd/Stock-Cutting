fbc821fd0d9b5fdbcb802bf549f471758fe467f3
Miller, Stuart (S&T-Student) <sm67c@mst.edu>
2017-09-12 17:05:54 -0500

    Updated graphs to match specifications given in class. They're now generated using MATLAB, rather than Excel.

---
+++ doc/test.m
@@ -0,0 +1,28 @@
+clear;
+
+filename = 'log_instance3.txt';
+
+rawData = importdata(filename);
+evals = rawData.data(:,1);
+fitness = rawData.data(:,2);
+
+[max, maxIdx] = max(fitness);
+
+minIdx = maxIdx;
+while(fitness(minIdx) ~= 0)
+    minIdx = minIdx - 1;
+end
+
+x = [evals(minIdx:maxIdx); 1000];
+y = [fitness(minIdx:maxIdx); fitness(maxIdx)];
+
+fig = figure;
+set(fig, 'Visible', 'off');
+
+stairs(x,y);
+title('Evals vs. Fitness for Best Solution');
+xlabel('Evaluation');
+ylabel('Fitness');
+axis([0 1000 0 fitness(maxIdx)+1]);
+
+saveas(fig, ['images\graph_' filename(1:end-4) '.png']);
