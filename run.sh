47aa2dad67a99f7f5df689aac926f60cbea28b81
Miller <sm67c@rc37xcs213.managed.mst.edu>
2017-09-23 18:46:39 -0500

    Updated run.sh

--- run.sh
+++ run.sh
@@ -7,28 +7,9 @@ if [ $# -eq 0 ]
 	exit 0
 fi
 
-# Determine which input file to use
-shapefile=""
-case $2 in
-	"problem1" | "1" | "instance1")
-		shapefile="patterns/50Shapes.txt"
-		;;
-    "pattern2" | "2" | "instance2")
-        shapefile="patterns/100Shapes.txt"
-		;;
-	"pattern3" | "3" | "instance3")
-		shapefile="patterns/100ShapesComplex.txt"
-		;;
-	*)
-		echo "Error: Please supply a valid problem instance as argument 2."
-		exit 0
-		;;
-esac
-
 # Compile project
 # Note: Compiling with C++11 to make use of high_resolution_clock for microsecond seeding
 g++ src/*.cpp -std=c++11 -o stock_cutting.exe
 
 # Run with given configuration
-echo $shapefile
-./stock_cutting.exe $1 $shapefile
+./stock_cutting.exe $1 $2
